94fcb40ee4d91240d918a59af1f71291bdea289e
inwoo0109 <80755964+inwoo0109@users.noreply.github.com>
2021-04-25 21:41:10 +0900

    Add files via upload

--- app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
+++ app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
@@ -2,13 +2,36 @@ package com.example.a2021swp_weatherwear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class RecommendActivity extends AppCompatActivity {
 
+    ImageButton btnAddCloset;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recommend);
+        setContentView(R.layout.activity_main);
+
+        btnAddCloset = findViewById(R.id.imageButton1);
+
+        btnAddCloset.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent i1 = new Intent(RecommendActivity.this, SelectActivity.class);
+                //이미지 상의 + 버튼을 누르면 SelectActivity 화면으로 이동한다.
+                startActivity(i1);
+            }
+        });
+
     }
+
+
+
 }
