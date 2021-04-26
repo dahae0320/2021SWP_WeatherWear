0976915687885a9df7bf21c62483fa78b63bea20
dahae0320 <ldh990320ldh@gmail.com>
2021-04-26 11:57:30 +0900

    recommend 화면 에러 해결

--- app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
+++ app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
@@ -15,7 +15,7 @@ public class RecommendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recommend);
 
         btnAddCloset = findViewById(R.id.imageButton1);
 
