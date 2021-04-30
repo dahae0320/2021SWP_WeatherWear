f3e79017345604882ecef6e4eb9d93f5ae2732db
dahae0320 <ldh990320ldh@gmail.com>
2021-04-30 22:29:54 +0900

    fragment viewpage2로 수정

--- app/src/main/java/com/example/a2021swp_weatherwear/SelectActivity.java
+++ app/src/main/java/com/example/a2021swp_weatherwear/SelectActivity.java
@@ -1,73 +1,59 @@
 package com.example.a2021swp_weatherwear;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 
-public class SelectActivity extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
-    private Button btnTapOuter, btnTapTop, btnTapBottom;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
-    private final int FRAGMENTOUTER = 1;
-    private final int FRAGMENTTOP = 2;
-    private final int FRAGMENTBOTTOM = 3;
+
+public class SelectActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+    Context mContext;
+
+    private ViewPager2 mViewPager;
+    private ViewPageAdaptor myPagerAdapter;
+    private TabLayout tabLayout;
+
+    private String[] titles = new String[]{"Outer", "Top", "Bottom"};
+
+    String code;
+    private BackPressHandler backPressHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
 
-        // 탭 버튼에 대한 참조
-        btnTapOuter = findViewById(R.id.btnOuter);
-        btnTapTop = findViewById(R.id.btnTop);
-        btnTapBottom = findViewById(R.id.btnBottom);
+        mContext = SelectActivity.this;
+        backPressHandler = new BackPressHandler(this);
 
-        // 탭 버튼 클릭 리스너 연결
-        btnTapOuter.setOnClickListener(this);
-        btnTapTop.setOnClickListener(this);
-        btnTapBottom.setOnClickListener(this);
+        //code = getIntent().getExtras().getString("code"); // 다른 Activity에서 값을 넘겨 받았을 때
+        code = "";
+        Log.e(TAG, code);
 
-        // 기본 fragment 설정 == 처음 호출시 화면에 뜨는 fragment
-        callFragment(FRAGMENTOUTER);
+        Fragment frag1 = new SelectOuterFragment().newInstance(code,"");
+        Fragment frag2 = new SelectTopFragment().newInstance(code,"https://m.naver.com");
+        Fragment frag3 = new SelectBottomFragment().newInstance(code,"");
+
+        mViewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        myPagerAdapter = new ViewPageAdaptor(this);
+        myPagerAdapter.addFrag(frag1);
+        myPagerAdapter.addFrag(frag2);
+        myPagerAdapter.addFrag(frag3);
+
+        mViewPager.setAdapter(myPagerAdapter);
+
+        //displaying tabs
+        new TabLayoutMediator(tabLayout, mViewPager, (tab, position) -> tab.setText(titles[position])).attach();
     }
 
-
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case (R.id.btnOuter):
-                callFragment(FRAGMENTOUTER);
-                break;
-            case (R.id.btnTop):
-                callFragment(FRAGMENTTOP);
-                break;
-            case (R.id.btnBottom):
-                callFragment(FRAGMENTBOTTOM);
-                break;
-        }
-    }
-
-    private void callFragment(int fragment_no) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        switch (fragment_no) {
-            case (1):   // outer 프래그먼트 호출
-                SelectOuterFragment fragmentOuter = new SelectOuterFragment();
-                transaction.replace(R.id.fragment_container, fragmentOuter);
-                transaction.commit();
-                break;
-            case (2):   // top 프래그먼트 호출
-                SelectTopFragment fragmentTop = new SelectTopFragment();
-                transaction.replace(R.id.fragment_container, fragmentTop);
-                transaction.commit();
-                break;
-            case (3):   // bottom 프래그먼트 호출
-                SelectBottomFragment fragmentBottom = new SelectBottomFragment();
-                transaction.replace(R.id.fragment_container, fragmentBottom);
-                transaction.commit();
-                break;
-        }
-    }
 }
