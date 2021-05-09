2d157817d512928a5fbe6102d8ca67859a8d3fa0
inwoo0109 <80755964+inwoo0109@users.noreply.github.com>
2021-05-09 21:09:32 +0900

    Add files via upload

---
+++ a2021swp_weatherwear/ViewPageAdaptor.java
@@ -0,0 +1,34 @@
+package com.example.a2021swp_weatherwear;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPageAdaptor extends FragmentStateAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public ViewPageAdaptor(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    public void addFrag(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        // Fragment 교체를 보여주는 처리 구현
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        // ViewPager 로 보여줄 View의 전체 개수
+        return mFragmentList.size();
+    }
+}
