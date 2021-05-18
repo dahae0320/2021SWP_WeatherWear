5bbfd50e73c323b840b7fe0d134453d2f238699f
inwoo0109 <80755964+inwoo0109@users.noreply.github.com>
2021-05-18 21:02:04 +0900

    Add files via upload

--- app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
+++ app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
@@ -4,30 +4,31 @@ package com.example.a2021swp_weatherwear;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-import com.github.clans.fab.FloatingActionButton;
-import com.github.clans.fab.FloatingActionMenu;
-
-import org.json.JSONException;
-
 
 public class RecommendActivity extends AppCompatActivity {
     FloatingActionMenu fabMenu;
     FloatingActionButton fabCloset;
     FloatingActionButton fabLikelist;
+    TextView weatherTemp;
+
+    private String year, month, day;
 
 
     private String strNick;
@@ -88,16 +89,53 @@ public class RecommendActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             Calendar calendar = Calendar.getInstance(); // 날짜 변수
+                            year = String.valueOf(calendar.get(Calendar.YEAR));
+                            month = 0+String.valueOf(calendar.get(Calendar.MONTH)+1);
+                            day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
                             int hour = calendar.get(Calendar.HOUR_OF_DAY); // 시
-                            if (hour <= 11)
+
+                            JSONObject r1 = apiTest.s1;
+                            JSONObject r2 = apiTest.s2;
+                            JSONObject r3 = apiTest.s3;
+                            JSONObject r4 = apiTest.s4;
+                            JSONObject r5 = apiTest.s5;
+
+                            System.out.println(year + month + day);
+
+
+                            if (hour <= 11) {
                                 timer.setText("현재 시각\n" + "오전" + hour + "시 ");
-                            else if (hour >= 13)
+                                weatherTemp = findViewById(R.id.txtBeforeCelsius1);
+                                weatherTemp.setText( r1 + "'C");
+                                weatherTemp = findViewById(R.id.txtBeforeCelsius2);
+                                weatherTemp.setText( r2 + "'C");
+                                weatherTemp = findViewById(R.id.txtBeforeCelsius3);
+                                weatherTemp.setText( r3 + "'C");
+                                weatherTemp = findViewById(R.id.txtBeforeCelsius4);
+                                weatherTemp.setText( r4 + "'C");
+                                weatherTemp = findViewById(R.id.tv);
+                                weatherTemp.setText( r5 + "'C");
+
+                            }
+                            else if (hour >= 13) {
                                 timer.setText("현재 시각\n" + "오후" + (hour - 12) + "시 ");
+                                weatherTemp = findViewById(R.id.txtBeforeCelsius1);
+                                weatherTemp.setText( r1 + "'C");
+                                weatherTemp = findViewById(R.id.txtBeforeCelsius2);
+                                weatherTemp.setText( r2 + "'C");
+                                weatherTemp = findViewById(R.id.txtBeforeCelsius3);
+                                weatherTemp.setText( r3 + "'C");
+                                weatherTemp = findViewById(R.id.txtBeforeCelsius4);
+                                weatherTemp.setText( r4 + "'C");
+                                weatherTemp = findViewById(R.id.tv);
+                                weatherTemp.setText( r5 + "'C");
+                            }
                         }
                     });
 
+
                     try {
-                        apiTest.func();
+                        apiTest.func(year, month, day);
                         Thread.sleep(1000); // 1000 ms = 1초
                     } catch (InterruptedException e) {
                         e.printStackTrace();
--- app/src/main/java/com/example/a2021swp_weatherwear/apiTest.java
+++ app/src/main/java/com/example/a2021swp_weatherwear/apiTest.java
@@ -1,7 +1,5 @@
 package com.example.a2021swp_weatherwear;
 
-import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,41 +9,132 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Calendar;
 
-public class apiTest extends Thread {
-    public void func() throws IOException, JSONException {
+
+public class apiTest extends Thread
+{
+    public JSONObject s1;
+    public JSONObject s2;
+    public JSONObject s3;
+    public JSONObject s4;
+    public JSONObject s5;
+
+    public void func(String year, String month, String day) throws IOException, JSONException
+    {
         // 기상청 동네예보 api에서 동네예보조회 요청에 필요한 매개변수들
-        String endPoint = "http://apis.data.go.kr/1360000/VilageFcstInfoService";
-        String serviceKey = "n9Pfhnwdrxh%2FiMJefGgPTp2AqXB6JERmRhzBvdbZHL7Cbneqc7N5j6TxUvNOis9Ri%2Fz0dFdM8jbOYhKcmWj2Qg%3D%3D";
+        String endPoint = "http://apis.data.go.kr/1360000/VilageFcstInfoService/";
+        String serviceKey = "sCyFo%2FDKOLW9l1EHk%2BlA0zwanD%2FpAjUbgIhgU5erOQKEcKoh6PUjl%2B%2FQ1WARH4AksQh3sl6FU%2Ff9buPEMK20Rw%3D%3D";
         String pageNo = "1";
-        String numOfRows = "14";
-        String baseDate = "20210511"; // 원하는 날짜
-        String baseTime = "2000"; // 원하는 시간
-        String nx = "35"; // 위경도임.
-        String ny = "128"; // 위경도 정보는 api 문서 볼 것
+        String numOfRows = "10";
+        String baseDate = year + month + day; // 원하는 날짜
+        String baseTime1 = "1500"; // 원하는 시간
+        String baseTime2 = "1700"; // 원하는 시간
+        String baseTime3 = "1900"; // 원하는 시간
+        String baseTime4 = "2100"; // 원하는 시간
+        String nx = "81"; // 위경도임.
+        String ny = "75"; // 위경도 정보는 api 문서 볼 것
+
+        Calendar calendar = Calendar.getInstance(); // 날짜 변수
+        int hour = calendar.get(Calendar.HOUR_OF_DAY); // 시
 
         // endpoint와 원하는 정보(동네예보조회:getVilageFcst), 서비스키와 기타 요청 매개변수들을 다 합친 쿼리 스트링이다.
-        String s = endPoint + "/getVilageFcst?serviceKey=" + serviceKey
+        String s1 = endPoint + "/getVilageFcst?serviceKey=" + serviceKey
                 + "&pageNo=" + pageNo
                 + "&numOfRows=" + numOfRows
                 + "+&dataType=JSON"
                 + "&base_date=" + baseDate
-                + "&base_time=" + baseTime
+                + "&base_time=" + baseTime1
                 + "&nx=" + nx
                 + "&ny=" + ny;
 
+        String s2 = endPoint + "/getVilageFcst?serviceKey=" + serviceKey
+                + "&pageNo=" + pageNo
+                + "&numOfRows=" + numOfRows
+                + "+&dataType=JSON"
+                + "&base_date=" + baseDate
+                + "&base_time=" + baseTime2
+                + "&nx=" + nx
+                + "&ny=" + ny;
+
+        String s3 = endPoint + "/getVilageFcst?serviceKey=" + serviceKey
+                + "&pageNo=" + pageNo
+                + "&numOfRows=" + numOfRows
+                + "+&dataType=JSON"
+                + "&base_date=" + baseDate
+                + "&base_time=" + baseTime3
+                + "&nx=" + nx
+                + "&ny=" + ny;
+
+        String s4 = endPoint + "/getVilageFcst?serviceKey=" + serviceKey
+                + "&pageNo=" + pageNo
+                + "&numOfRows=" + numOfRows
+                + "+&dataType=JSON"
+                + "&base_date=" + baseDate
+                + "&base_time=" + baseTime4
+                + "&nx=" + nx
+                + "&ny=" + ny;
+
+        String s5 = endPoint + "/getVilageFcst?serviceKey=" + serviceKey
+                + "&pageNo=" + pageNo
+                + "&numOfRows=" + numOfRows
+                + "+&dataType=JSON"
+                + "&base_date=" + baseDate
+                + "&base_time=" + hour
+                + "&nx=" + nx
+                + "&ny=" + ny;
+
+
         // Http연결을한다. 호출 Method는 "GET"이다.
-        URL url = new URL(s);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
+        URL url1 = new URL(s1);
+
+        HttpURLConnection conn1 = (HttpURLConnection) url1.openConnection();
+        conn1.setRequestMethod("GET");
+
+        URL url2 = new URL(s2);
+
+        HttpURLConnection conn2 = (HttpURLConnection) url2.openConnection();
+        conn2.setRequestMethod("GET");
+
+        URL url3 = new URL(s3);
+
+        HttpURLConnection conn3 = (HttpURLConnection) url3.openConnection();
+        conn3.setRequestMethod("GET");
+
+        URL url4 = new URL(s4);
+
+        HttpURLConnection conn4 = (HttpURLConnection) url4.openConnection();
+        conn4.setRequestMethod("GET");
+
+        URL url5 = new URL(s5);
+
+        HttpURLConnection conn5 = (HttpURLConnection) url5.openConnection();
+        conn4.setRequestMethod("GET");
+
 
         // BufferedReader를 이용해서 리턴값을 받고 StringBuilder를 이용해서 라인마다 한줄씩 추가하여 저장한다.
-        BufferedReader bufferedReader = null;
-        if (conn.getResponseCode() == 200) {
-            bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        } else {
-            //connection error :(
+
+        BufferedReader bufferedReader;
+        if(conn1.getResponseCode() >= 200 && conn1.getResponseCode() <= 300) {
+            bufferedReader = new BufferedReader(new InputStreamReader(conn1.getInputStream()));
         }
+        else if(conn2.getResponseCode() >= 200 && conn2.getResponseCode() <= 300) {
+            bufferedReader = new BufferedReader(new InputStreamReader(conn2.getInputStream()));
+        }
+        else if(conn3.getResponseCode() >= 200 && conn3.getResponseCode() <= 300) {
+            bufferedReader = new BufferedReader(new InputStreamReader(conn3.getInputStream()));
+        }
+        else if(conn4.getResponseCode() >= 200 && conn4.getResponseCode() <= 300) {
+            bufferedReader = new BufferedReader(new InputStreamReader(conn4.getInputStream()));
+        }
+        else if(conn5.getResponseCode() >= 200 && conn5.getResponseCode() <= 300) {
+            bufferedReader = new BufferedReader(new InputStreamReader(conn5.getInputStream()));
+        }
+        else {
+            bufferedReader = new BufferedReader(new InputStreamReader(conn1.getErrorStream()));
+        }
+
+
         StringBuilder stringBuilder = new StringBuilder();
         String line;
         while ((line = bufferedReader.readLine()) != null) {
@@ -53,16 +142,20 @@ public class apiTest extends Thread {
         }
         bufferedReader.close();
         String result = stringBuilder.toString();
-        conn.disconnect();
-
+        conn1.disconnect();
+        conn2.disconnect();
+        conn3.disconnect();
+        conn4.disconnect();
         JSONObject mainObject = new JSONObject(result);
         System.out.println(mainObject);
         JSONArray itemArray = mainObject.getJSONObject("response").getJSONObject("body").getJSONObject("items").getJSONArray("item");
+
         for (int i = 0; i < itemArray.length(); i++) {
             JSONObject item = itemArray.getJSONObject(i);
             String category = item.getString("category");
             String value = item.getString("fcstValue");
             System.out.println(category + "  " + value);
         }
+
     }
 }
