b196d64b3bc4eb9879aa286177a7a9d6745fbad1
dahae0320 <ldh990320ldh@gmail.com>
2021-05-23 17:08:53 +0900

    옷차림 추천 - 관련 DB 접근

--- app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
+++ app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
@@ -9,7 +9,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
@@ -18,11 +20,17 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RecommendActivity extends AppCompatActivity {
     FloatingActionMenu fabMenu;
@@ -30,15 +38,16 @@ public class RecommendActivity extends AppCompatActivity {
     FloatingActionButton fabLikelist;
     TextView weatherTemp;
 
+    // 현재 날짜
     private String year, month, day;
+
+    // 옷차림 추천 관련 변수들
+    private int currentCel = 22; // 현재 기온 변수 (임의로 지정함)
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
 
     private String strNick;
     long systemTime = System.currentTimeMillis();
-    // 현재 시스템 시간 구하기
-    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
-    // 출력 형태를 위한 formmater
-    String dTime = formatter.format(systemTime);
-    // format에 맞게 출력하기 위한 문자열 변환
 
     TextView timer;
 
@@ -46,6 +55,12 @@ public class RecommendActivity extends AppCompatActivity {
     TextView home_text;
     String weather_data;
     String home_data;
+
+    // 현재 시스템 시간 구하기
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.KOREA);
+    // 출력 형태를 위한 formmater
+    String dTime = formatter.format(systemTime);
+    // format에 맞게 출력하기 위한 문자열 변환
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +74,24 @@ public class RecommendActivity extends AppCompatActivity {
         fabMenu = findViewById(R.id.fabMenu);
         fabCloset = findViewById(R.id.fabCloset);
         fabLikelist = findViewById(R.id.fabLikelist);
+
+        fabCloset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1 = new Intent(RecommendActivity.this, SelectActivity.class);
+                //이미지 상의 의류 추가 버튼을 누르면 SelectActivity 화면으로 이동한다.
+                startActivity(i1);
+            }
+        });
+
+        fabLikelist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i3 = new Intent(RecommendActivity.this, LikeActivity.class);
+                //이미지 상의 좋아요 확인 버튼을 누르면 SelectActivity 화면으로 이동한다.
+                startActivity(i3);
+            }
+        });
 
         // 좋아요 버튼
         final Button favBtn = findViewById(R.id.favBtn);
@@ -96,7 +129,7 @@ public class RecommendActivity extends AppCompatActivity {
 
                             Calendar calendar = Calendar.getInstance(); // 날짜 변수
                             year = String.valueOf(calendar.get(Calendar.YEAR));
-                            month = 0+String.valueOf(calendar.get(Calendar.MONTH)+1);
+                            month = 0 + String.valueOf(calendar.get(Calendar.MONTH) + 1);
                             day = String.valueOf(calendar.get(Calendar.DAY_OF_MONTH));
                             int hour = calendar.get(Calendar.HOUR_OF_DAY); // 시
 
@@ -105,25 +138,24 @@ public class RecommendActivity extends AppCompatActivity {
                             if (hour <= 11) {
                                 timer.setText("현재 시각\n" + "오전" + hour + "시 ");
                                 weatherTemp = findViewById(R.id.txtBeforeCelsius1);
-                                weatherTemp.setText( r1 + "'C");
+                                weatherTemp.setText(r1 + "'C");
                                 weatherTemp = findViewById(R.id.txtBeforeCelsius2);
-                                weatherTemp.setText( r2 + "'C");
+                                weatherTemp.setText(r2 + "'C");
                                 weatherTemp = findViewById(R.id.txtBeforeCelsius3);
-                                weatherTemp.setText( r3 + "'C");
+                                weatherTemp.setText(r3 + "'C");
                                 weatherTemp = findViewById(R.id.txtBeforeCelsius4);
-                                weatherTemp.setText( r4 + "'C");
+                                weatherTemp.setText(r4 + "'C");
 
-                            }
-                            else if (hour >= 13) {
+                            } else if (hour >= 13) {
                                 timer.setText("현재 시각\n" + "오후" + (hour - 12) + "시 ");
                                 weatherTemp = findViewById(R.id.txtBeforeCelsius1);
-                                weatherTemp.setText( r1 + "'C");
+                                weatherTemp.setText(r1 + "'C");
                                 weatherTemp = findViewById(R.id.txtBeforeCelsius2);
-                                weatherTemp.setText( r2 + "'C");
+                                weatherTemp.setText(r2 + "'C");
                                 weatherTemp = findViewById(R.id.txtBeforeCelsius3);
-                                weatherTemp.setText( r3 + "'C");
+                                weatherTemp.setText(r3 + "'C");
                                 weatherTemp = findViewById(R.id.txtBeforeCelsius4);
-                                weatherTemp.setText( r4 + "'C");
+                                weatherTemp.setText(r4 + "'C");
                             }
                         }
                     });
@@ -137,27 +169,64 @@ public class RecommendActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
 
-                    fabCloset.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent i1 = new Intent(RecommendActivity.this, SelectActivity.class);
-                            //이미지 상의 의류 추가 버튼을 누르면 SelectActivity 화면으로 이동한다.
-                            startActivity(i1);
-                        }
-                    });
-
-                    fabLikelist.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent i3 = new Intent(RecommendActivity.this, LikeActivity.class);
-                            //이미지 상의 좋아요 확인 버튼을 누르면 SelectActivity 화면으로 이동한다.
-                            startActivity(i3);
-                        }
-                    });
                 }
             }
         };
         thread.start();
+
+        recommendGarment(currentCel);
+    }
+
+    // 옷차림 추천 메소드
+    private void recommendGarment(int currentCel) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<String> outer = new ArrayList<>();
+        ArrayList<String> top = new ArrayList<>();
+        ArrayList<String> bottom = new ArrayList<>();
+        final int[] current = new int[1];
+
+        firebaseDatabase = FirebaseDatabase.getInstance();  // 파이어베이스 DB 연동
+        databaseReference = firebaseDatabase.getReference("GarmentTemperature");   // DB 데이블 연결
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                arrayList.clear();
+                int i = 0;
+
+                // 현재 기온에 맞는 추천 옷차림 테이블 찾
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    arrayList.add(Integer.valueOf(dataSnapshot.getKey()));
+                    if ( arrayList.get(i) >= currentCel ) {
+                        current[0] = i - 1;
+                        System.out.println(current[0]);
+                        break;
+                    }
+                    i++;
+                }
+
+                // 현재 기온에 맞는 추천 옷차림을 따로 배열에 할당
+                for(DataSnapshot dataSnapshot : snapshot.child(String.valueOf(current[0])).child("Outer").getChildren() ) {
+//                    System.out.println(dataSnapshot.getValue());
+                    outer.add((String) dataSnapshot.getValue());
+                }
+                for(DataSnapshot dataSnapshot : snapshot.child(String.valueOf(current[0])).child("Top").getChildren() ) {
+//                    System.out.println(dataSnapshot.getValue());
+                    top.add((String) dataSnapshot.getValue());
+                }
+                for(DataSnapshot dataSnapshot : snapshot.child(String.valueOf(current[0])).child("Bottom").getChildren() ) {
+//                    System.out.println(dataSnapshot.getValue());
+                    bottom.add((String) dataSnapshot.getValue());
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void showToast(String message) {
