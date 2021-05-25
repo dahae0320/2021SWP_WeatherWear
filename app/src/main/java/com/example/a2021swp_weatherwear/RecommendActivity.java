dc1a7094d9532195d6616c2484fcdf5cd80fb73b
inwoo0109 <80755964+inwoo0109@users.noreply.github.com>
2021-05-25 19:50:46 +0900

    Add files via upload

--- app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
+++ app/src/main/java/com/example/a2021swp_weatherwear/RecommendActivity.java
@@ -11,12 +11,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
 
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -47,11 +41,10 @@ public class RecommendActivity extends AppCompatActivity {
     String weather_data2;
     String weather_data3;
     String weather_data4;
-    
+
     //최고 최저
     String weather_high;
     String weather_low;
-
 
 
     @Override
@@ -115,104 +108,125 @@ public class RecommendActivity extends AppCompatActivity {
         time3 = findViewById(R.id.txtBeforeTime3);
         time4 = findViewById(R.id.txtBeforeTime4);
 
+
         String now = new DataLoader().DateLoader(-1);
         String getDate = now.substring(0, 8);
 
         int min = Integer.parseInt(now.substring(11, 13));
         String getTime;
-
+        String getTime1;
+        String getTime2;
+        String getTime3;
+        String getTime4;
 
         if (min >= 30) {
             Date mDate = new Date(System.currentTimeMillis());
             SimpleDateFormat simpleDate = new SimpleDateFormat("HH00");
+            SimpleDateFormat simpleDate1 = new SimpleDateFormat("HH02");
+            SimpleDateFormat simpleDate2 = new SimpleDateFormat("HH04");
+            SimpleDateFormat simpleDate3 = new SimpleDateFormat("HH06");
+            SimpleDateFormat simpleDate4 = new SimpleDateFormat("HH08");
+
             getTime = simpleDate.format(mDate);
+            getTime1 = simpleDate1.format(mDate);
+            getTime2 = simpleDate2.format(mDate);
+            getTime3 = simpleDate3.format(mDate);
+            getTime4 = simpleDate4.format(mDate);
         }
         else {
             getTime = now.substring(9, 11) + "00";
+            getTime1 = now.substring(9, 11) + "02";
+            getTime2 = now.substring(9, 11) + "04";
+            getTime3 = now.substring(9, 11) + "06";
+            getTime4 = now.substring(9, 11) + "08";
+
         }
 
         System.out.println(getDate);
         System.out.println(getTime);
+        System.out.println(getTime1);
+        System.out.println(getTime2);
+        System.out.println(getTime3);
+        System.out.println(getTime4);
 
         Thread thread = new Thread() {
 
             @Override
             public void run() {
                 while (!isInterrupted()) {
-                    Calendar calendar = Calendar.getInstance(); // 날짜 변수
-                    int hour = calendar.get(Calendar.HOUR_OF_DAY); // 시
 
-                    if (hour == 12) {
-                        timer.setText("현재 시각\n" + "오후 12시 ");
-                    } else if (hour == 0) {
-                        timer.setText("현재 시각\n" + "오전 12시 ");
-                    } else if (hour >= 13) {
-                        timer.setText("현재 시각\n" + "오후 " + (hour - 12) + "시 ");
-                    } else {
-                        timer.setText("현재 시각\n" + "오전 " + hour + "시 ");
-                    }
-
-                    // 2시간뒤 시각
-                    if (hour <= 9) {
-                        time1.setText("오전 " + (hour + 2) + "시");
-                    } else if (hour == 10) {
-                        time1.setText("오후 " + 12 + "시");
-                    } else if (hour == 22) {
-                        time1.setText("오전 " + 12 + "시");
-                    } else if (hour >= 23) {
-                        time1.setText("오전 " + (hour - 22) + "시");
-                    } else {
-                        time1.setText("오후 " + (hour - 10) + "시");
-                    }
-                    // 4시간 뒤 시간
-                    if (hour <= 7) {
-                        time2.setText("오전 " + (hour + 4) + "시");
-                    } else if (hour == 8) {
-                        time2.setText("오후 " + 12 + "시");
-                    } else if (hour == 20) {
-                        time2.setText("오전 " + 12 + "시");
-                    } else if (hour >= 21) {
-                        time2.setText("오전 " + (hour - 20) + "시");
-                    } else {
-                        time2.setText("오후 " + (hour - 8) + "시");
-                    }
-
-                    //6시간 뒤 시각
-                    if (hour <= 5) {
-                        time3.setText("오전 " + (hour + 6) + "시");
-                    } else if (hour == 6) {
-                        time3.setText("오후 " + 12 + "시");
-                    } else if (hour == 18) {
-                        time3.setText("오전 " + 12 + "시");
-                    } else if (hour >= 19) {
-                        time3.setText("오전 " + (hour - 18) + "시");
-                    } else {
-                        time3.setText("오후 " + (hour - 6) + "시");
-                    }
-
-                    // 8시간 뒤 시각
-                    if (hour <= 3) {
-                        time4.setText("오전 " + (hour + 8) + "시");
-                    } else if (hour == 4) {
-                        time4.setText("오후 " + 12 + "시");
-                    } else if (hour == 16) {
-                        time4.setText("오전 " + 12 + "시");
-                    } else if (hour >= 17) {
-                        time4.setText("오전 " + (hour - 16) + "시");
-                    } else {
-                        time4.setText("오후 " + (hour - 4) + "시");
-                    }
-
-                    weather_data = getWeatherXmlData(getDate, getTime);
-                    weather_data1 = getWeatherXmlData(getDate, getTime+2);
-                    weather_data2 = getWeatherXmlData(getDate, getTime+4);
-                    weather_data3 = getWeatherXmlData(getDate, getTime+6);
-                    weather_data4 = getWeatherXmlData(getDate, getTime+8);
-
+                   weather_data = WeatherApi.getWeatherData(getDate, getTime);
+                    weather_data1 = WeatherApi.getWeatherData(getDate, getTime1);
+                    weather_data2 = WeatherApi.getWeatherData(getDate, getTime2);
+                    weather_data3 = WeatherApi.getWeatherData(getDate, getTime3);
+                    weather_data4 = WeatherApi.getWeatherData(getDate, getTime4);
 
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
+                            Calendar calendar = Calendar.getInstance(); // 날짜 변수
+                            int hour = calendar.get(Calendar.HOUR_OF_DAY); // 시
+
+                            if (hour == 12) {
+                                timer.setText("현재 시각\n" + "오후 12시 ");
+                            } else if (hour == 0) {
+                                timer.setText("현재 시각\n" + "오전 12시 ");
+                            } else if (hour >= 13) {
+                                timer.setText("현재 시각\n" + "오후 " + (hour - 12) + "시 ");
+                            } else {
+                                timer.setText("현재 시각\n" + "오전 " + hour + "시 ");
+                            }
+                            // 2시간뒤 시각
+                            if (hour <= 9) {
+                                time1.setText("오전 " + (hour + 2) + "시");
+                            } else if (hour == 10) {
+                                time1.setText("오후 " + 12 + "시");
+                            } else if (hour == 22) {
+                                time1.setText("오전 " + 12 + "시");
+                            } else if (hour >= 23) {
+                                time1.setText("오전 " + (hour - 22) + "시");
+                            } else {
+                                time1.setText("오후 " + (hour - 10) + "시");
+                            }
+                            // 4시간 뒤 시간
+                            if (hour <= 7) {
+                                time2.setText("오전 " + (hour + 4) + "시");
+                            } else if (hour == 8) {
+                                time2.setText("오후 " + 12 + "시");
+                            } else if (hour == 20) {
+                                time2.setText("오전 " + 12 + "시");
+                            } else if (hour >= 21) {
+                                time2.setText("오전 " + (hour - 20) + "시");
+                            } else {
+                                time2.setText("오후 " + (hour - 8) + "시");
+                            }
+
+                            //6시간 뒤 시각
+                            if (hour <= 5) {
+                                time3.setText("오전 " + (hour + 6) + "시");
+                            } else if (hour == 6) {
+                                time3.setText("오후 " + 12 + "시");
+                            } else if (hour == 18) {
+                                time3.setText("오전 " + 12 + "시");
+                            } else if (hour >= 19) {
+                                time3.setText("오전 " + (hour - 18) + "시");
+                            } else {
+                                time3.setText("오후 " + (hour - 6) + "시");
+                            }
+
+                            // 8시간 뒤 시각
+                            if (hour <= 3) {
+                                time4.setText("오전 " + (hour + 8) + "시");
+                            } else if (hour == 4) {
+                                time4.setText("오후 " + 12 + "시");
+                            } else if (hour == 16) {
+                                time4.setText("오전 " + 12 + "시");
+                            } else if (hour >= 17) {
+                                time4.setText("오전 " + (hour - 16) + "시");
+                            } else {
+                                time4.setText("오후 " + (hour - 4) + "시");
+                            }
+
                             weather_text.setText(weather_data + "°C");
                             weather_text1.setText(weather_data1 + "°C");
                             weather_text2.setText(weather_data2 + "°C");
@@ -220,83 +234,46 @@ public class RecommendActivity extends AppCompatActivity {
                             weather_text4.setText(weather_data4 + "°C");
                             //최고 최저 텍스트 출력 및 날씨 정보 출력
 
-                            if()
-                            {
-                                weather_text5.setText();
-                            }
+                            /*
+                                String s = "";
+                            Document doc;
+                                NodeList nodeList = doc.getElementsByTagName("data");
+
+                                for(int i = 0; i< nodeList.getLength(); i++) {
+
+                                    //날씨 데이터를 추출
+                                    s += "" + " ";
+
+                                    Node node = nodeList.item(i); //data엘리먼트 노드
+                                    Element fstElmnt = (Element) node;
+                                    NodeList nameList = fstElmnt.getElementsByTagName("tmn");
+                                    Element nameElement = (Element) nameList.item(0);
+                                    nameList = nameElement.getChildNodes();
+
+
+                                    NodeList timeList = fstElmnt.getElementsByTagName("tmEf");
+                                    s += timeList.item(0).getChildNodes().item(0).getNodeValue() + ", ";
+
+
+                                    s += "최저온도 = " + ((Node) nameList.item(0)).getNodeValue() + " ,";
+
+                                    NodeList highList = fstElmnt.getElementsByTagName("tmx");
+                                    //<wfKor>맑음</wfKor> =====> <wfKor> 태그의 첫번째 자식노드는 TextNode 이고 TextNode의 값은 맑음
+                                    s += "최고온도 = " + highList.item(0).getChildNodes().item(0).getNodeValue() + " ,";
+
+                                    NodeList websiteList = fstElmnt.getElementsByTagName("wf");
+                                    //<wfKor>맑음</wfKor> =====> <wfKor> 태그의 첫번째 자식노드는 TextNode 이고 TextNode의 값은 맑음
+                                    s += "날씨 = " + websiteList.item(0).getChildNodes().item(0).getNodeValue() + "\n";
+                                }
+
+                                weather_text5.setText(s);
+
+                             */
                         }
                     });
                 }
             }
         };
         thread.start();
-    }
-
-    String getWeatherXmlData(String getDate, String getTime){
-        StringBuffer buffer = new StringBuffer();
-
-        String endPoint = "http://apis.data.go.kr/1360000/VilageFcstInfoService/";
-        String serviceKey = "sCyFo%2FDKOLW9l1EHk%2BlA0zwanD%2FpAjUbgIhgU5erOQKEcKoh6PUjl%2B%2FQ1WARH4AksQh3sl6FU%2Ff9buPEMK20Rw%3D%3D";
-        String pageNo = "1";
-        String numOfRows = "10";
-        String baseDate = getDate; // 원하는 날짜
-        String baseTime = getTime;
-        String nx = "81"; // 위경도임.
-        String ny = "75";
-
-        String queryUrl = endPoint + "getUltraSrtNcst?serviceKey=" + serviceKey
-                + "&pageNo=" + pageNo
-                + "&numOfRows=" + numOfRows
-                + "&base_date=" + baseDate
-                + "&base_time=" + baseTime
-                + "&nx=" + nx
-                + "&ny=" + ny;
-
-        try {
-            URL url = new URL(queryUrl);
-
-            InputStream is = url.openStream();
-
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            XmlPullParser xpp = factory.newPullParser();
-            xpp.setInput(new InputStreamReader(is, "UTF-8"));
-
-            String t;
-            int i = 0;
-            xpp.next();
-            int eventType = xpp.getEventType();
-
-            while (eventType != XmlPullParser.END_DOCUMENT) {
-                switch (eventType) {
-                    case XmlPullParser.START_DOCUMENT:
-                        break;
-                    case XmlPullParser.START_TAG:
-                        t = xpp.getName();
-
-                        if (t.equals("item")) ;
-                        else if (t.equals("obsrValue")) {
-                            i++;
-                            //기온
-                            if (i == 4) {
-                                buffer.append("");
-                                xpp.next();
-                                buffer.append(xpp.getText());
-                                System.out.println(xpp.getText());
-                            }
-                        }
-                        break;
-                    case XmlPullParser.TEXT:
-                        break;
-                    case XmlPullParser.END_TAG:
-                        t = xpp.getName(); //태그 이름 얻어오기
-                        if (t.equals("item")) ;
-                        break;
-                }
-                eventType = xpp.next();
-            }
-        } catch (Exception e) {
-
-        }
-        return buffer.toString();
     }
 }
